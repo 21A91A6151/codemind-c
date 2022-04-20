7bbe0d7eb19cd42a4dcfdc8530d2a0b4f3fd0a37
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-04-20 23:04:25 +0530

    #include<stdio.h> int main() {    int n,arr[100][100],i,j,k;    scanf("%d",&n);    for(i=0;i<n;i++)    {        for(j=0;j<2;j++)        {            scanf("%d",&arr[i][j]);        }    }   for(i=0;i<n;i++)    {        k=0;        for(j=0;j<2;j++)        {           k=k+arr[i][j];        }        printf("%d ",k);    }    return 0; }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,a,b,c;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d%d",&a,&b);
-        c=a+b;
-        printf("%d
-",c);
-    }
-    return 0;
+   int n,arr[100][100],i,j,k;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<2;j++)
+       {
+           scanf("%d",&arr[i][j]);
+       }
+   }
+  for(i=0;i<n;i++)
+   {
+       k=0;
+       for(j=0;j<2;j++)
+       {
+          k=k+arr[i][j];
+       }
+       printf("%d
+",k);
+   }
+   return 0;
 }
